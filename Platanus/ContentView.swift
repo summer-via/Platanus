8848edd4818c570cfc71d0b2af1ff021834bfad3
hewenfeng <hewenfeng@sensetime.com>
2021-10-09 18:29:48 +0800

    Initial Commit

---
+++ Platanus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Platanus
+//
+//  Created by hewenfeng on 2021/10/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Platanus/PlatanusApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlatanusApp.swift
+//  Platanus
+//
+//  Created by hewenfeng on 2021/10/9.
+//
+
+import SwiftUI
+
+@main
+struct PlatanusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
